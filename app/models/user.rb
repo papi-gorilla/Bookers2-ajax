2932d99cfdfce018bdfa70b56b362514d46c9bae
EC2 Default User <ec2-user@ip-172-31-17-110.ap-northeast-1.compute.internal>
2021-05-05 23:27:56 +0000

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+  has_many :favorites ,dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
+  validates :name, uniqueness: true
+  validates :name, length: {in: 2..20}
+  validates :introduction, length: {maximum: 50}
+
+  attachment :profile_image
+
+end
